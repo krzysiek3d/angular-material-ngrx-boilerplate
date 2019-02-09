323b234b7a3e074486309c393ac4043ee28d9ea0
Krzysztof Kacik <krzysiek3d@gmail.com>
2019-02-09 10:18:45 +0100

    Fix layout, remove feauters module

--- src/app/features/components/index.ts
+++
@@ -1,5 +0,0 @@
-import { FeatureComponent } from './feature/feature.component';
-
-export const components: any[] = [FeatureComponent];
-
-export * from './feature/feature.component';
--- src/app/features/containers/index.ts
+++
@@ -1,5 +0,0 @@
-import { FeaturesComponent } from './features/features.component';
-
-export const containers: any[] = [FeaturesComponent];
-
-export * from './features/features.component';
